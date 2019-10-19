f984fe06e3b7d9d0e8bbe69388ffa9ba3c56213d
Albert Liu <albertymliu@gmail.com>
2019-10-19 12:29:40 -0400

    almost working with eb

--- application.py
+++ application.py
@@ -1,1 +1,8 @@
 from app import app
+
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    app.debug = True
+    app.run()
