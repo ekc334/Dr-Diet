19b6303beecf43340519ae00b07b1bcd17f6cb2d
Albert Liu <albertymliu@gmail.com>
2019-10-20 07:41:02 -0400

    finished MVP UI

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def add_cors_header(resp):
+    resp.headers.add('Access-Control-Allow-Origin', '*')
+    return resp
+
+app.after_request(add_cors_header)
+
 from app import routes
+
