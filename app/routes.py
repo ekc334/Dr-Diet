092e3b64434789225b2a08637440a58fed3d9501
aashere <48105539+aashere@users.noreply.github.com>
2019-10-20 03:44:20 -0400

    modified routes

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ import asyncio
 @app.route('/')
 @app.route('/index')
 def index():
-    allergens = 'cheese'
+    allergens = request.args.get('dishName')
     rn = "grilled cheese sandwich"
     loop = asyncio.new_event_loop()
     return str(loop.run_until_complete(allergen_search(allergens, rn)))
