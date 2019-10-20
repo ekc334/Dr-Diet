4d9e2c1b845851ea3c765b04c87d32fc1d691e0d
Albert Liu <albertymliu@gmail.com>
2019-10-20 06:25:28 -0400

    added query parameters

--- script.js
+++ script.js
@@ -4,23 +4,23 @@ $("form").on('submit', function (e) {
   //ajax call here
   const dish = $('#first').val();
   const allergy = $('#second').val();
-  fetch(url).then(function(response) {
-    response.text().then(function(text) {
-      alert(text);
-    });
-  });
-  // $.ajax({    
-  //   type: 'GET',
-  //   url: url, //- action form
-  //   data: {allergenName:allergy,dishName:dish},
-  //   dataType: "jsonp",
-  //   success: function(){
-  //     alert('success');
-  //   },
-  //   failure: function() {
-  //     alert('fail')
-  //   }
+  // fetch(url).then(function(response) {
+  //   response.text().then(function(text) {
+  //     alert(text);
+  //   });
   // });
+  $.ajax({    
+    type: 'GET',
+    url: url, //- action form
+    data: {allergenName:allergy,dishName:dish},
+    dataType: "jsonp",
+    success: function(){
+      alert('success');
+    },
+    failure: function() {
+      alert('fail')
+    }
+  });
   //stop form submission
   e.preventDefault();
 });
