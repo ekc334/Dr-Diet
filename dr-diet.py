10cc97d89bbb2368e2dfe463916f4460a531136f
Albert Liu <albertymliu@gmail.com>
2019-10-20 01:30:41 -0400

    added async!

--- dr-diet.py
+++ dr-diet.py
@@ -1,4 +1,5 @@
 import ssl, os
+import asyncio
 
 from bs4 import BeautifulSoup
 
@@ -6,6 +7,7 @@ from boundio import run_tasks
 
 import urllib.parse
 import urllib.request
+import aiohttp
 
 import requests
 
@@ -41,42 +43,48 @@ def parse_recipe_data(soup):
     ingredients = [i.get_text() for i in ingredients]
     return [i for i in ingredients if i and i != "Add all ingredients to list"]
 
-def search(query_dict, k):
+async def search(session, query_dict, k):
     url = base_url + urllib.parse.urlencode(query_dict) + "&page="+str(k)
 
-    response = requests.get(url, headers={ 'Cookie':'euConsent=true' })
+    async with session.get(url, headers = { 'Cookie':'euConsent=true' }) as resp:
+        text = await resp.text()
 
-    soup = BeautifulSoup(response.text, 'html.parser')
-
+    soup = BeautifulSoup(text, 'html.parser')
     return parse_catalog_data(soup)
 
-def get_recipe(url):
-    response = requests.get(url, headers={ 'Cookie':'euConsent=true' })
+async def get_recipe(session, url):
+    print(f"get_recipe url is: {url}")
+    async with session.get(url, headers = { 'Cookie':'euConsent=true' }) as resp:
+        url_escaped = url.replace("\\", '').replace("/", '').replace(':', '')
+        text = await resp.text()
+        with open(f'data/{url_escaped}.html', 'w') as f:
+            f.write(text.replace('\\n', '\n'))
 
-    url_escaped = url.replace("\\", '').replace("/", '').replace(':', '')
-    with open(f'data/{url_escaped}.html', 'w') as f:
-        f.write(response.text.replace('\\n', '\n'))
-
-    soup = BeautifulSoup(response.text, 'html.parser')
-    return parse_recipe_data(soup)
+        soup = BeautifulSoup(text, 'html.parser')
+        return parse_recipe_data(soup)
 
 
-def allergen_search(allergen, recipe_name):
+async def allergen_search(allergen, recipe_name):
     query_options = {
         "wt": recipe_name,
         "sort" : "re"
     }
     allergen_count = 0
     total_count = 0
-    for pageNumber in range(1,3):
-        query_result = search(query_options, pageNumber)
+    async with aiohttp.ClientSession() as session:
+        searches = []
+        for n in range(1, 5):
+            searches.append(search(session, query_options, n))
 
-        for result in query_result:
+        results = []
+        for result in await asyncio.gather(*searches):
+            results += result
+
+        recipe_searches = [get_recipe(session, result['url']) for result in results]
+        recipes = await asyncio.gather(*recipe_searches)
+
+        for detailed_recipe in recipes:
             present = False
-            recipe_url = result['url']
-            print(recipe_url)
-            detailed_recipe = get_recipe(recipe_url)
-
             total_count += 1
             if detailed_recipe:
                 for ingredient in detailed_recipe:
@@ -89,9 +97,13 @@ def allergen_search(allergen, recipe_name):
     retval = allergen_count/total_count
     return retval
 
+async def print_async(boi):
+    print(await boi)
+
 if (not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None)):
         ssl._create_default_https_context = ssl._create_unverified_context
 
 allergens = 'cheese'
 rn = "grilled cheese sandwich"
-print(allergen_search(allergens, rn))
+
+print(run_tasks(print_async(allergen_search(allergens, rn))))
