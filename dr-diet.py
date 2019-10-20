339021b9005cb4f8ab28b76fcaaebf152a7b1e2c
aashere <48105539+aashere@users.noreply.github.com>
2019-10-20 00:37:22 -0400

    made changes

--- dr-diet.py
+++ dr-diet.py
@@ -18,7 +18,10 @@ def parse_catalog_data(soup):
     articles = soup.findAll("article", {"class": "fixed-recipe-card"})
 
     iterarticles = iter(articles)
-    next(iterarticles)
+    try:
+        next(iterarticles)
+    except:
+        return
     for article in iterarticles:
         data = {}
         try:
@@ -33,32 +36,28 @@ def parse_catalog_data(soup):
     return search_data
 
 def parse_recipe_data(soup):
-    ingredients = soup.findAll("li", {"class": "checkList__line"})
-
-    data = {
-        "ingredients": [],
-    }
-
-    for ingredient in ingredients:
-        string = ingredient.find("span", {"class": "recipe-ingred_txt"}).get_text()
-        if string and string != "Add all ingredients to list":
-            data["ingredients"].append(string)
-
-    return data
+    ingredients = soup.findAll("span", {"class": "recipe-ingred_txt"})
+    ingredients += soup.findAll("span", {"class": "ingredients-item-name"})
+    ingredients = [i.get_text() for i in ingredients]
+    return [i for i in ingredients if i and i != "Add all ingredients to list"]
 
 async def search(query_dict, k):
     url = base_url + urllib.parse.urlencode(query_dict) + "&page="+str(k)
 
     response = requests.get(url, headers={ 'Cookie':'euConsent=true' })
 
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.text, 'html.parser')
 
     return parse_catalog_data(soup)
 
 async def get_recipe(url):
     response = requests.get(url, headers={ 'Cookie':'euConsent=true' })
 
-    soup = BeautifulSoup(response.content, 'html.parser')
+    url_escaped = url.replace("\\", '').replace("/", '').replace(':', '')
+    with open(f'data/{url_escaped}.html', 'w') as f:
+        f.write(response.text.replace('\\n', '\n'))
+
+    soup = BeautifulSoup(response.text, 'html.parser')
     return parse_recipe_data(soup)
 
 
@@ -68,32 +67,24 @@ async def allergen_search(allergen, recipe_name):
         "sort" : "re"
     }
     allergen_count = 0
-    total_count = 0 
+    total_count = 0
     for pageNumber in range(1,3):
         query_result = await search(query_options, pageNumber)
-       
-        rangeNumber = 0
-        if (pageNumber==1):
-                rangeNumber = min(16,len(query_result))
-        else:
-                rangeNumber = min(4,len(query_result))
-        for item in range(rangeNumber):
+
+        for result in query_result:
             present = False
-            print("item=",item, "rangeNumber=", rangeNumber)
-           
-            recipe_url = query_result[item]['url']
+            recipe_url = result['url']
             print(recipe_url)
             detailed_recipe = await get_recipe(recipe_url)
-     
+
             total_count += 1
-            if(len(detailed_recipe['ingredients']) != 0):
-                for ingredient in detailed_recipe['ingredients']:
-                    truth = allergen in ingredient
-                    if (truth):                       
-                        present = True
+            if detailed_recipe:
+                for ingredient in detailed_recipe:
+                     present = present or allergen in ingredient
                 if(present):
-                    print((16*(pageNumber-1) + item))
+                    print(f"ITEM NO.: {total_count}")
                     allergen_count += 1
+                    print(f"ALLERGEN COUNT: {allergen_count}")
     print("allergen count is", allergen_count)
     retval = allergen_count/total_count
     return retval
@@ -102,5 +93,5 @@ if (not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unveri
         ssl._create_default_https_context = ssl._create_unverified_context
 
 allergens = 'cheese'
-rn = "yakisoba"
-run_tasks(allergen_search(allergens, rn))
+rn = "grilled cheese sandwich"
+print(run_tasks(allergen_search(allergens, rn)))
